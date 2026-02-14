518a69ce84cdf0e622bdf4bbbd31d32101ead530
Naseer Pasha <n763122562@gmail.com>
2026-02-14 14:59:08 +0530

    feat: integrate authentic Spider-Man and unify all hero alignments

--- app/src/components/3d/SpiderManModel.jsx
+++ app/src/components/3d/SpiderManModel.jsx
@@ -1,27 +1,41 @@
-import { Box } from '@react-three/drei'
+import React, { useLayoutEffect } from 'react'
+import { useLoader } from '@react-three/fiber'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { TextureLoader } from 'three'
+import * as THREE from 'three'
 
 export function SpiderManModel(props) {
+  const obj = useLoader(OBJLoader, 'models/spiderman/M-FF_iOS_HERO_Miles_Morales_Spider-Man_Ultimate.obj')
+  const texture = useLoader(TextureLoader, 'models/spiderman/M-FF_iOS_HERO_Miles_Morales_Spider-Man_Ultimate_Body_D.png')
+
+  useLayoutEffect(() => {
+    obj.traverse((child) => {
+      if (child.isMesh) {
+        child.material.map = texture
+        child.material.needsUpdate = true
+      }
+    })
+
+    const box = new THREE.Box3().setFromObject(obj)
+    const center = box.getCenter(new THREE.Vector3())
+    const size = box.getSize(new THREE.Vector3())
+    
+    // Normalize scale to height of 5.5 units to match Iron Man/Hulk exactly
+    const targetHeight = 5.5
+    const scaleFactor = targetHeight / size.y
+    obj.scale.setScalar(scaleFactor)
+
+    // Position normalization
+    obj.position.x = -center.x * scaleFactor
+    obj.position.y = -center.y * scaleFactor
+    obj.position.z = -center.z * scaleFactor
+
+  }, [obj, texture])
+
   return (
     <group {...props}>
-      {/* Body */}
-      <Box args={[1.5, 2, 1]} position={[0, 0, 0]}>
-        <meshStandardMaterial color="#b91c1c" />
-      </Box>
-      {/* Legs/Hips */}
-      <Box args={[1.5, 0.5, 1]} position={[0, -1.25, 0]}>
-        <meshStandardMaterial color="#1d4ed8" />
-      </Box>
-      {/* Head */}
-      <Box args={[1.2, 1, 1]} position={[0, 1.5, 0]}>
-        <meshStandardMaterial color="#b91c1c" />
-      </Box>
-      {/* Eyes */}
-      <Box args={[0.3, 0.3, 0.1]} position={[0.3, 1.6, 0.5]}>
-        <meshStandardMaterial color="white" />
-      </Box>
-      <Box args={[0.3, 0.3, 0.1]} position={[-0.3, 1.6, 0.5]}>
-        <meshStandardMaterial color="white" />
-      </Box>
+      <primitive object={obj} position={[0, 1.2, 0]} />
     </group>
   )
 }
+
